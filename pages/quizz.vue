f9aa4af9e5db83fee993d8e1c9eac32f7f6b024d
Arthur Brody <Arthur.Brody@ensc.fr>
2024-01-15 22:18:13 +0100

    quizz.vue

--- pages/quizz.vue
+++ pages/quizz.vue
@@ -3,7 +3,7 @@
   <div class=" h-80 shadow-lg m-5 max-w-full">
     <ul class="flex flex-nowrap space-x-2">
       <li v-for="(picture, id) in pictures" :key="id">
-        <img :src="picture" @click="selectedPicture = id" 
+        <img :src="picture.src" @click="selectedPicture = id" 
         class="cursor-pointer hover:opacity-90 transition-all border-2 border-solid" :class="{'border-teal-500': selectedPicture === id}">
         <div class="text-center cursor-pointer hover:opacity-90 transition-all bg-teal-100 mt-3" 
         @click="selectedAnswer = id" :class="{'bg-teal-500 text-white': selectedAnswer === id}">
@@ -14,7 +14,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang = "ts">
 
 import {ref, watchEffect} from 'vue'
 
@@ -24,10 +24,10 @@ const selectedAnswer = ref(-1)
 
 
 const pictures = [
-'https://via.placeholder.com/150x150',
-'https://via.placeholder.com/160x150',
-'https://via.placeholder.com/170x150',
-'https://via.placeholder.com/180x150',
+  { src : 'https://via.placeholder.com/150x150', answer : '150' },
+  { src : 'https://via.placeholder.com/160x150', answer : '160' },
+  { src : 'https://via.placeholder.com/170x150', answer : '170' },
+  { src : 'https://via.placeholder.com/180x150', answer : '180' },
 ]
 
 const answers = [
@@ -37,19 +37,12 @@ const answers = [
   '160',
 ]
 
-const combinations = [
-  [0, 1],
-  [1, 3],
-  [2, 0],
-  [3, 2],
-]
 
 watchEffect(() => {
-  console.log(selectedPicture.value, selectedAnswer.value)
-  if (combinations.includes([selectedPicture.value, selectedAnswer.value])) {
-    console.log('correct')
+  if (answers[selectedAnswer.value] === pictures[selectedPicture.value].answer) {
+    alert('Good answer')
   } else {
-    console.log('wrong')
+    alert('Bad answer')
   }
 })
 
