668fe0f485b455f48fa43524e14943fbc2873294
Arthur Brody <Arthur.Brody@ensc.fr>
2024-01-16 10:07:01 +0100

    quizz.vue

--- pages/quizz.vue
+++ pages/quizz.vue
@@ -1,13 +1,18 @@
 
 <template>
   <div class=" h-80 shadow-lg m-5 max-w-full">
-    <ul v-auto-animate class="flex flex-nowrap space-x-2">
-      <li v-for="(picture, id) in pictures" :key="id">
-        <img :src="picture.src" @click="selectedPicture = id" 
-        class="cursor-pointer hover:opacity-90 transition-all border-3 border-solid" :class="{'border-teal-500': selectedPicture === id}">
-        <div class="text-center cursor-pointer hover:opacity-90 transition-all bg-teal-800 mt-3" 
-        @click="selectedAnswer = id" :class="{'bg-teal-500 text-white': selectedAnswer === id}">
-          {{ answers[id] }}
+    <ul v-auto-animate="{ duration: 700 }" class="flex flex-nowrap space-x-2">
+      <li v-for="picture in pictures" :key="picture.src">
+        <img :src="picture.src" @click="selectedPicture = picture.src"
+          class="cursor-pointer hover:opacity-90 transition-all border-2 border-solid"
+          :class="{ 'border-teal-500': selectedPicture === picture.src }">
+      </li>
+    </ul>
+    <ul v-auto-animate="{ duration: 700 }" class="flex flex-nowrap space-x-2">
+      <li v-for="answer in answers" :key="answer" @click="selectedAnswer = answer"
+        :class="{ 'bg-teal-500': selectedAnswer === answer }">
+        <div class="text-center cursor-pointer hover:opacity-90 transition-all bg-teal-800 mt-3">
+          {{ answer }}
         </div>
       </li>
     </ul>
@@ -16,18 +21,21 @@
 
 <script setup lang = "ts">
 
-import {ref, watchEffect} from 'vue'
+import { ref, watchEffect } from 'vue'
 
-const selectedPicture = ref(-1)
-const selectedAnswer = ref(-1)
+const selectedPicture = ref('')
+const selectedAnswer = ref('')
 
-
+interface Picture {
+  src: string
+  answer: string
+}
 
 const pictures = ref([
-  { src : 'https://via.placeholder.com/180x150', answer : '180' },
-  { src : 'https://via.placeholder.com/160x150', answer : '160' },
-  { src : 'https://via.placeholder.com/170x150', answer : '170' },
-  { src : 'https://via.placeholder.com/150x150', answer : '150' },
+  { src: 'https://via.placeholder.com/180x150', answer: '180' },
+  { src: 'https://via.placeholder.com/160x150', answer: '160' },
+  { src: 'https://via.placeholder.com/170x150', answer: '170' },
+  { src: 'https://via.placeholder.com/150x150', answer: '150' },
 ])
 
 const answers = ref([
@@ -38,20 +46,20 @@ const answers = ref([
 ])
 
 const checkAnswer = () => {
-  if (answers.value[selectedAnswer.value] === pictures.value[selectedPicture.value].answer) {
-   // alert('Good answer')
+  if (selectedAnswer.value === pictures.value.find(picture => picture.src === selectedPicture.value)?.answer) {
+    // alert('Good answer')
     // sort pictures by src
     pictures.value.sort((a, b) => a.src.localeCompare(b.src))
   } else {
     alert('Bad answer')
   }
-selectedPicture.value=-1
-selectedAnswer.value=-1
+  selectedPicture.value = ''
+  selectedAnswer.value = ''
 }
 
 
 watchEffect(() => {
-  if (selectedPicture.value !== -1 && selectedAnswer.value !== -1) {
+  if (selectedPicture.value && selectedAnswer.value) {
     checkAnswer()
   }
 })
