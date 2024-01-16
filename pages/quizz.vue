de8f86960423cfe18cbeb556a9e8a39f8a32a6b6
Arthur Brody <Arthur.Brody@ensc.fr>
2024-01-16 10:33:29 +0100

    quizz.vue

--- pages/quizz.vue
+++ pages/quizz.vue
@@ -1,14 +1,15 @@
 
 <template>
   <div class=" h-80 shadow-lg m-5 max-w-full">
-    <ul v-auto-animate="{ duration: 700 }" class="flex flex-nowrap space-x-2">
-      <li v-for="picture in pictures" :key="picture.src">
+    <ul v-auto-animate class="flex flex-nowrap space-x-2">
+      <li v-for="picture in pictures" :key="picture.src" class = "relative">
+        <span v-if = "picture.found" class = "absolute w-full h-full bg-teal-700/40 z-10"></span> <!-- rep OK - empeche le click -->
         <img :src="picture.src" @click="selectedPicture = picture.src"
           class="cursor-pointer hover:opacity-90 transition-all border-2 border-solid"
           :class="{ 'border-teal-500': selectedPicture === picture.src }">
       </li>
     </ul>
-    <ul v-auto-animate="{ duration: 700 }" class="flex flex-nowrap space-x-2">
+    <ul v-auto-animate class="flex flex-nowrap space-x-2">
       <li v-for="answer in answers" :key="answer" @click="selectedAnswer = answer"
         :class="{ 'bg-teal-500': selectedAnswer === answer }">
         <div class="text-center cursor-pointer hover:opacity-90 transition-all bg-teal-800 mt-3">
@@ -32,10 +33,10 @@ interface Picture {
 }
 
 const pictures = ref([
-  { src: 'https://via.placeholder.com/180x150', answer: '180' },
-  { src: 'https://via.placeholder.com/160x150', answer: '160' },
-  { src: 'https://via.placeholder.com/170x150', answer: '170' },
-  { src: 'https://via.placeholder.com/150x150', answer: '150' },
+  { src: 'https://via.placeholder.com/180x150', answer: '180', found : false },
+  { src: 'https://via.placeholder.com/160x150', answer: '160', found : false },
+  { src: 'https://via.placeholder.com/170x150', answer: '170', found : false },
+  { src: 'https://via.placeholder.com/150x150', answer: '150', found : false },
 ])
 
 const answers = ref([
@@ -45,11 +46,16 @@ const answers = ref([
   '160',
 ])
 
-const checkAnswer = () => {
+const checkAnswer = async () => {
+  // for ts, check if selectedPicture.value is in pictures.value
+  const pic = pictures.value.find(picture => picture.src === selectedPicture.value)
+  if (!pic) return
   if (selectedAnswer.value === pictures.value.find(picture => picture.src === selectedPicture.value)?.answer) {
-    // alert('Good answer')
-    // sort pictures by src
-    pictures.value.sort((a, b) => a.src.localeCompare(b.src))
+    // set this picture to the beginning of the array
+    pictures.value = [pic, ...pictures.value.filter(picture => picture.src !== selectedPicture.value)]
+    // wait for 300ms - to see the green overlay
+    await new Promise(resolve => setTimeout(resolve, 300))
+    pic.found = true
   } else {
     alert('Bad answer')
   }
