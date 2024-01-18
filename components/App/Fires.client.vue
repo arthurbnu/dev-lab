7b7acec8d2df64038691697f0f975811a60b938a
arthurbnu <114090227+arthurbnu@users.noreply.github.com>
2024-01-18 09:50:11 +0100

    Update App

---
+++ components/App/Fires.client.vue
@@ -0,0 +1,36 @@
+<template>
+    <Fireworks
+    ref="fw"
+    v-if="mounted"
+    :autostart="false"
+    :options="options"
+      :style="{
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        position: 'fixed',
+        background: '#000'
+      }"
+    />
+  </template>
+
+  <script lang="ts" setup>
+ import { ref, watch } from 'vue'
+import { Fireworks } from '@fireworks-js/vue'
+  import type { FireworksOptions } from '@fireworks-js/vue'
+  
+  const fw = ref<InstanceType<typeof Fireworks>>()
+  const options = ref<FireworksOptions>({ opacity: 0.5 })
+  
+  async function startFireworks() {
+    if (!fw.value) return
+    fw.value.start()
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    await fw.value.waitStop()
+  }
+  
+  watch(fw, () => startFireworks())
+  </script>
+
+ 
