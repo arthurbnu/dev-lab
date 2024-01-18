9418335ceacdee96d08267630d3e5bae496d1620
Arthur Brody <Arthur.Brody@ensc.fr>
2024-01-18 17:04:50 +0100

    fireworks.vue

--- pages/quizz.vue
+++ pages/quizz.vue
@@ -1,41 +1,34 @@
 
 <template>
-    <main class="min-h-screen">
+  <main class="min-h-screen">
     <AppHeader class="mb-1 text-center" :title="title" :description="description" />
 
-  <div class=" h-80 shadow-lg m-5 max-w-full">
-    <ul v-auto-animate class="flex flex-nowrap space-x-2 mb-4">
-      <li v-for="picture in pictures" :key="picture.src" class="relative" :style="basisStyle">
-        <!-- span de fond - empeche le click si deja ok -->
-        <span v-if="picture.found" class="absolute w-full h-full bg-teal-700/40 z-10 transition-all my-height grid place-content-center text-4xl">
-          🥇
-        </span>
-        <img :src="'quizz/' + picture.src" @click="selectedPicture = picture.src" :data-src="picture.src"
-          draggable="true" @dragstart="dragStart"
-          class="cursor-pointer hover:opacity-90 transition-all border-2 border-solid"
-          :class="{ 'border-teal-500': selectedPicture === picture.src, 'my-error': lastError.picture === picture.src }">
-      </li>
-    </ul>
-    <ul v-auto-animate class="flex flex-nowrap space-x-2">
-      <li v-for="answer in answers" :key="answer" @click="selectedAnswer = answer"
-        class="grid place-content-center cursor-pointer hover:opacity-90 transition-all bg-teal-900 h-20 basis-1 border-2 border-solid"
-        :class="{ 'border-teal-500': selectedAnswer === answer, 'my-error': lastError.answer === answer }"
-        :data-answer="answer"
-        @dragover="dragOver" @drop="drop"
-        :style="basisStyle">
-        <span class='text-sm'>
-          {{ answer }}
-        </span>
-      </li>
-    </ul>
-  </div>
-  <!-- <div class="text-center">
-    <button @click="pictures.value = pictures.value.sort(() => Math.random() - 0.5)" class="bg-teal-900 text-white px-4 py-2 rounded-full hover:bg-teal-700 transition-all">
-      Mélanger
-    </button>
-  </div> -->
-</main>
-<AppFires v-if = "startFireWorks || test" />
+    <div class=" h-80 shadow-lg m-5 max-w-full">
+      <ul v-auto-animate class="flex flex-nowrap space-x-2 mb-4">
+        <li v-for="picture in pictures" :key="picture.src" class="relative" :style="basisStyle">
+          <!-- span de fond - empeche le click si deja ok -->
+          <span v-if="picture.found"
+            class="absolute w-full h-full bg-teal-700/40 z-10 transition-all my-height grid place-content-center text-4xl">
+            🥇
+          </span>
+          <img :src="'quizz/' + picture.src" @click="selectedPicture = picture.src" :data-src="picture.src"
+            draggable="true" @dragstart="dragStart"
+            class="cursor-pointer hover:opacity-90 transition-all border-2 border-solid"
+            :class="{ 'border-teal-500': selectedPicture === picture.src, 'my-error': lastError.picture === picture.src }">
+        </li>
+      </ul>
+      <ul v-auto-animate class="flex flex-nowrap space-x-2">
+        <li v-for="answer in answers" :key="answer" @click="selectedAnswer = answer"
+          class="grid place-content-center cursor-pointer hover:opacity-90 transition-all bg-teal-900 h-20 border-2 border-solid text-sm"
+          :class="{ 'border-teal-500': selectedAnswer === answer, 'my-error': lastError.answer === answer }"
+          :data-answer="answer" @dragover="dragOver" @drop.prevent="drop" @dragenter="dragOver" @dragLeave.prevent="dragLeave" 
+          :style="basisStyle">
+            {{ answer }}
+        </li>
+      </ul>
+    </div>
+  </main>
+  <AppFires v-if="startFireWorks || test" class = "opacity-60"/>
 </template>
 
 <script setup lang = "ts">
@@ -45,29 +38,23 @@ import { ref, watchEffect } from 'vue'
 const startFireWorks = ref(false)
 const test = ref(false)
 
-const title  = "L'IPNI selon l'Intelligence Artificielle";
-const description = "Essaye d'associer chaque image au bon informaticien.";
+const title = "L'IPNI selon l'Intelligence Artificielle";
+const description = "Essaye de retrouver qui est qui !";
 
-const dragged = ref(null) as Ref<HTMLElement | null>
+// const dragged = ref() as Ref<HTMLImageElement>
+  // type DragElt = DragEvent & { target: HTMLElement }
+const dragged = ref() as Ref<HTMLElement>
+const dragStart = (e: DragEvent) => dragged.value = e.target as HTMLElement
+const dragOver = (e: DragEvent) => e.preventDefault()
+const dragEnter = (e: DragEvent) => (e.target as HTMLElement).classList.add('border-teal-500')
+const dragLeave = (e: DragEvent) => (e.target as HTMLElement).classList.remove('border-teal-500')
 
-  const dragStart = (e: DragEvent) => {
-    console.log('dragStart', e.target)
-    dragged.value = e.target as HTMLElement
-  }
-
-  const dragOver = (e: DragEvent) => {
-    e.preventDefault()
-  }
-
-  const drop = (e: DragEvent) => {
-    e.preventDefault()
-    console.log('drop', 'drop target is ', e.target, 'dragged is ', dragged.value)
-    if (!dragged.value) return
-    selectedPicture.value = dragged.value.getAttribute('data-src') as string
-    selectedAnswer.value = e.target?.getAttribute('data-answer') as string
-    
-  }
-
+const drop = (e: DragEvent) => {
+  if (!dragged.value) return
+  selectedAnswer.value = (e.target as HTMLElement).dataset.answer as string
+  selectedPicture.value = dragged.value.dataset.src as string
+  (e.target as HTMLElement).classList.remove('border-teal-500')
+}
 
 useSeoMeta({
   title: title,
@@ -132,8 +119,8 @@ const checkAnswer = async () => {
     // pause avant animation 
     await new Promise(resolve => setTimeout(resolve, 300))
     pic.found = true
-test.value= true
-  } 
+    test.value = true
+  }
   else {    // mauvaise réponse
     lastError.value = {
       picture: selectedPicture.value,
@@ -150,22 +137,21 @@ test.value= true
 }
 
 watchEffect(() => {
-  if (selectedPicture.value && selectedAnswer.value) 
+  if (selectedPicture.value && selectedAnswer.value)
     checkAnswer()
-   if (pictures.value.every(picture => picture.found)) 
- startFireWorks.value = true
+  if (pictures.value.every(picture => picture.found))
+    startFireWorks.value = true
 })
 
 </script>
 
 <style>
-.my-override-container {   
+.my-override-container {
   max-width: 100vw;
 }
 </style>
 
 <style scoped lang="scss">
-
 .my-height {
   height: calc(100% + 110px);
 }
@@ -178,35 +164,44 @@ watchEffect(() => {
   0% {
     transform: translate(1px, 1px) rotate(0deg);
   }
+
   10% {
     transform: translate(-1px, -2px) rotate(-1deg);
   }
+
   20% {
     transform: translate(-3px, 0px) rotate(1deg);
   }
+
   30% {
     transform: translate(3px, 2px) rotate(0deg);
   }
+
   40% {
     transform: translate(1px, -1px) rotate(1deg);
   }
+
   50% {
     transform: translate(-1px, 2px) rotate(-1deg);
   }
+
   60% {
     transform: translate(-3px, 1px) rotate(0deg);
   }
+
   70% {
     transform: translate(3px, 1px) rotate(-1deg);
   }
+
   80% {
     transform: translate(-1px, -1px) rotate(1deg);
   }
+
   90% {
     transform: translate(1px, 2px) rotate(0deg);
   }
+
   100% {
     transform: translate(1px, -2px) rotate(-1deg);
   }
-}
-</style>
+}</style>
