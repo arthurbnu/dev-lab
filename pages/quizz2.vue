bc2b64411782e08acee5ddf9148857076914645b
Arthur Brody <Arthur.Brody@ensc.fr>
2024-01-20 14:54:01 +0100

    memoire - quizz2

--- pages/quizz2.vue
+++ pages/quizz2.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <main class="min-h-screen transition-all duration-700" :class = "{'opacity-0' : !ready}" ref="main">
+  <main class="min-h-screen transition-all duration-700" :class="{ 'opacity-0': !ready }" ref="main">
     <AppHeader class="mb-1 text-center" :title="title" :description="description" />
     <div class=" h-80 shadow-lg m-5 max-w-full">
       <!-- <ul v-auto-animate class="flex flex-nowrap space-x-2 mb-4"> -->
@@ -18,16 +18,34 @@
           </li>
         </transition-group>
       </VueDraggableNext>
-          <ul v-auto-animate class="flex flex-nowrap space-x-2">
-          <li v-for="answer in answers" :key="answer"
-            class="grid place-content-center cursor-pointer hover:opacity-90 transition-all bg-teal-900 h-20 border-4 border-teal-700 border-solid text-sm"
-            :class="{ 'my-error': lastError.answer === answer }" :data-answer="answer" :style="basisStyle">
-            {{ answer }}
-          </li>
+      <ul v-auto-animate class="flex flex-nowrap space-x-2">
+        <li v-for="answer in answers" :key="answer"
+          class="grid place-content-center cursor-pointer hover:opacity-90 transition-all bg-teal-900 h-20 border-4 border-teal-700 border-solid text-sm"
+          :class="{ 'my-error': lastError.answer === answer }" :data-answer="answer" :style="basisStyle">
+          {{ answer }}
+        </li>
       </ul>
     </div>
   </main>
-  <AppFires v-if="startFireWorks" class="opacity-60" />
+  <div v-if="!ready" class="absolute inset-0 flex justify-center items-center">
+    <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-teal-900"></div>
+  </div>
+  <div v-if = "youWin">
+    <AppFires class="opacity-60" />
+    <!-- Bien joué + bouton Rejouer qui actualise -->
+    <div class="absolute inset-0 flex justify-center items-center">
+      <div class="bg-white/90 rounded-lg shadow-lg p-5">
+        <h1 class="text-2xl font-bold text-center">Bravo !</h1>
+        <p class="text-center">Tu as réussi à remettre tout le monde à sa place !</p>
+        <div class="flex justify-center mt-5">
+          <ULink to="/quizz" class="bg-teal-900 text-white px-3 py-2 rounded-lg shadow-lg hover:bg-teal-700 transition-all">
+            Rejouer
+          </ULink>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script setup lang = "ts">
@@ -51,11 +69,11 @@ useSeoMeta({
 });
 
 const ready = ref(false)
-const startFireWorks = computed(() => pictures.value.every(picture => picture.found))
+const youWin = computed(() => pictures.value.every(picture => picture.found))
 const main = ref() as Ref<HTMLElement>
 const lastError = ref({ picture: '', answer: '' })
 
-onMounted(() =>  ready.value=true);
+onMounted(() => ready.value = true);
 
 const handleChange = (e: any) => {
   console.log(e)
@@ -70,7 +88,7 @@ const handleChange = (e: any) => {
 
 // reset last error each time its value is not empty
 watchEffect(async () => {
-  if (lastError.value.picture || lastError.value.answer ) {
+  if (lastError.value.picture || lastError.value.answer) {
     await nextTick()
     lastError.value = { picture: '', answer: '' }
   }
