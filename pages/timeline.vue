644a6947d3568a9844171d6071f04128bc990779
arthurbnu <114090227+arthurbnu@users.noreply.github.com>
2024-01-29 22:28:23 +0100

    timeline.vue : frise chronologique

---
+++ pages/timeline.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="q-px-lg q-py-md">
+    <q-timeline color="secondary">
+      <q-timeline-entry heading body="Timeline heading" />
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+        :body="body"
+      />
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 21, 1986"
+        icon="delete"
+        :body="body"
+      />
+
+      <q-timeline-entry heading body="November, 2017" />
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        :body="body"
+      />
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        :body="body"
+      />
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        color="orange"
+        icon="done_all"
+        :body="body"
+      />
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        :body="body"
+      />
+
+      <q-timeline-entry
+        title="Event Title"
+        subtitle="February 22, 1986"
+        :body="body"
+      />
+    </q-timeline>
+  </div>
+</template>
