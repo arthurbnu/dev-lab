a090b26ce5719edfd9a731d6dffaeba89bf6a1c0
arthurbnu <114090227+arthurbnu@users.noreply.github.com>
2024-01-29 22:57:17 +0100

    Update timeline.vue

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -7,7 +7,7 @@
         title="ADELE"
         subtitle="2 mai"
 color="secondary"
-        avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+        avatar="https://s3.fr-par.scw.cloud/rdg-portal/logos-macarons/Macarons%20Recherche%20Data%20Gouv_Ateliers%20de%20la%20donn%C3%A9e.jpg"
         :body="body"
       />
 
@@ -18,23 +18,28 @@ color="secondary"
         :body="body"
       />
 
-      <q-timeline-entry heading body="November, 2017" />
+   
 
       <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
+        title="Sigi-AL"
+        subtitle="12 mai"
+        :body="body"
+icon="settings_accessibility"
+color="orange"
+      />
+
+<q-timeline-entry
+        title="Ateliers IA"
+        subtitle="15,16,17,19 mai"
+        icon="rocket_launch"
         :body="body"
       />
 
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :body="body"
-      />
+   
 
       <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
+        title="Wikidata, printemps de la donnée"
+        subtitle="découvrez comment alimenter et obtenir des données Wikidata avec le langage spatql, .. et bien plus encore"
         color="orange"
         icon="done_all"
         :body="body"
