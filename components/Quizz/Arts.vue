209c8f03abcabea181d8795433740f01d7ff70c7
Arthur Brody <Arthur.Brody@ensc.fr>
2024-02-04 16:47:05 +0100

    Add loading animation to QuizzDrag component

--- components/Quizz/Arts.vue
+++ components/Quizz/Arts.vue
@@ -6,7 +6,18 @@
       <p class="text-lg">Faites glisser chaque oeuvre vers son auteur</p>
     </div>
     <div v-if="error" class="text-red-500">{{ error }}</div>
-    <QuizzDrag :picsInit="pics"/>
+    <div v-if="pending">
+      <div class="animate-pulse flex space-x-4">
+        <div class="rounded-full bg-gray-200 h-12 w-12"></div>
+        <div class="flex-1 space-y-4 py-1">
+          <div class="space-y-2">
+            <div class="h-4 bg-gray-200 rounded w-3/4"></div>
+            <div class="h-4 bg-gray-200 rounded w-5/6"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <QuizzDrag v-else-if="pics" :picsInit="pics"/>
   </section>
 </template>
 
