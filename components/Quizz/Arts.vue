1c302cb53e7ef74ec7d68e202fb298ef10959755
Arthur Brody <Arthur.Brody@ensc.fr>
2024-02-04 15:49:50 +0100

    Quizz Arts

---
+++ components/Quizz/Arts.vue
@@ -0,0 +1,70 @@
+
+<template>
+  <section>
+    <div class="flex flex-col items-center space-y-4">
+      <h1 class="text-3xl font-bold">Quizz</h1>
+      <p class="text-lg">Trouvez l'auteur de chaque oeuvre</p>
+    </div>
+    <div class="flex flex-col items-center space-y-4">
+      <p class="text-lg">Chaque oeuvre est associée à un auteur</p>
+      <p class="text-lg">Faites glisser chaque oeuvre vers son auteur</p>
+    </div>
+    <!-- show error if ther is one -->
+    <div v-if="error" class="text-red-500">{{ error }}</div>
+    <QuizzDrag :picsInit="pics"/>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { watchEffect, ref } from 'vue';
+
+const sparqlQuery = `
+#  +800 résultats
+# doublons : plusieurs images, plusieurs auteurs
+# - sample img -> 848 résultats
+# - concat auteurs -> 819
+# données manquantes : 
+#  - auteurs inconnus (tête de méduse)
+
+#defaultView:ImageGrid
+#             wdt:P347 ?idJoconde.        # 80 oeuvres sur +800
+#             wdt:P973 ?descriptionUrl.   # 590
+SELECT ?peintureLabel ?mouvementLabel ?peinture 
+(MD5(CONCAT(str(?peinture),str(RAND()))) as ?random)
+(SAMPLE(?image) AS ?image) 
+(GROUP_CONCAT(DISTINCT ?artisteLabel; SEPARATOR = " | ") AS ?artisteLabels) WHERE {
+  ?peinture wdt:P31 wd:Q3305213;
+    wdt:P135 ?mouvement;
+    wdt:P18 ?image;
+    wdt:P170 ?artiste.
+  ?mouvement (wdt:P31|wdt:P361) wd:Q4692.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en".
+    ?artiste rdfs:label ?artisteLabel.
+  }
+}
+GROUP BY ?peintureLabel ?mouvementLabel ?peinture
+ORDER BY ?random
+# LIMIT 10
+`;
+
+const baseUrl = 'https://query.wikidata.org/sparql?query='
+
+const { data: items, error: error, pedning: pending } = await useFetch(baseUrl + encodeURIComponent(sparqlQuery))
+
+const pics = ref([]) as any
+
+watchEffect(() => {
+  console.log('items : ', items)
+  pics.value = items.results.bindings.map((item: any) => {
+    console.log('item : ', item)
+    return {
+      src: item.image.value,
+      answer: item.artisteLabels.value
+    }
+  })
+  console.log('pics : ', pics.value)
+})
+
+</script>
--- pages/test.vue
+++ pages/test.vue
@@ -1,33 +1,18 @@
 <template>
   <main class="min-h-screen">
     <AppHeader class="mb-1 text-center" :title="title" :description="description" />
-    <QuizzDrag :picsInit="picsInit"/> 
   </main>
 </template>
 
 <script setup>
 
-const title = "Test quizz";
-const description = "Sparql | Langage de requête pour les données liées rdf." +
-  " Les données de la requête sont issues de wikidata, et complétées par des données de dbpedia et l'api wikipedia.";
+const title = "Test ";
+const description = "Page test"
 
 useSeoMeta({
   title: title,
   description,
   author: "A B",
 });
-
-const picsInit = [
-  { src: 'art1.png', answer: 'Arthur'},
-  { src: 'art2.png', answer: 'Christophe'},
-  { src: 'art3.png', answer: 'Madeleine'},
-  { src: 'art4.png', answer: 'Ferdi'},
-  { src: 'art5.png', answer: 'Elisa'},
-  { src: 'art6.png', answer: 'Kevin'},
-  { src: 'art7.png', answer: 'Maël'},
-  { src: 'art8.png', answer: 'François'},
-  { src: 'art9.png', answer: 'Jimmy'},
-  { src: 'art10.png', answer: 'Mathilde'},
-]
 
 </script>
