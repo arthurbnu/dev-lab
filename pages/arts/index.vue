5c16a0eb035733ed55c16fe2cd0797aa99aa1f8c
Arthur Brody <Arthur.Brody@ensc.fr>
2024-02-04 16:07:08 +0100

    Fix bug in login functionality

---
+++ pages/arts/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <main class="min-h-screen">
+        <AppHeader class="mb-6" title="Arts visuels" :description="description" />
+        <div class="space-y-4">
+            <QuizzArts/>
+        </div>
+    </main>
+</template>
+
+<script setup>
+
+const description =  " ... Jeu sparql avec des peintures disponiles sur wikidata";
+
+useSeoMeta({
+    title: "Arts ",
+    description,
+});
+
+
+</script>
