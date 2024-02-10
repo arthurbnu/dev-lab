bd95f0652a6173605b5927ca06795ca4ff24e6b5
Arthur Brody <Arthur.Brody@ensc.fr>
2024-02-10 02:24:14 +0100

    Refactor drag and swap functionality in Quizz/DragSwap.vue

--- components/Quizz/DragSwap.vue
+++ components/Quizz/DragSwap.vue
@@ -7,9 +7,9 @@
         {{ orientationError }}
       </p>
       <div v-else class=" h-80 shadow-lg  max-w-full transition-all">
-        <VueDraggableNext :list="pictures" @end="handleEnd" animation="500" tag="ul" :move="handleMove"
+        <VueDraggableNext :list="pictures" @end="handleEnd" tag="ul" :move="handleMove" 
           class="flex flex-nowrap space-x-2 mb-4">
-          <transition-group>
+          <transition-group class="img-group" name="img-group">
             <li v-for="(picture, id) in pictures" :key="picture.src" class="relative" :style="basisStyle">
               <!-- <li v-for="(picture, id) in pictures" :key="picture.src" class="relative flex items-end" :style="basisStyle"> -->
               <span v-if="picture.found"
@@ -176,6 +176,15 @@ watchEffect(() => props.easyMode && sortFound())
 .my-override-container {
   max-width: 100vw;
 }
+
+.img-group * {
+  transition: all 0.5s ease;
+}
+
+.img-group-move {
+  transition: all 0.7s ease;
+}
+
 </style>
 
 <style scoped lang="scss">
