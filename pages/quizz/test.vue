8f4d8ef69d5118743f3cece79b1456e5da2ffb37
Arthur Brody <Arthur.Brody@ensc.fr>
2024-02-10 01:44:14 +0100

    Remove unused code for handling drag and drop

---
+++ pages/quizz/test.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <main class="min-h-screen">
+    <AppHeader class="mb-5 text-center" :title="title" :description="description" />
+    <QuizzDragSwap :picsInit="picsInit" />
+  </main>
+</template>
+
+<script setup lang = "ts">
+
+const title = "L'IPNI selon l'Intelligence Artificielle";
+const description = "Tout est dans le désordre.. Essaye de remettre chaque image au dessus du bon prénom !";
+
+useSeoMeta({
+  // title: title,
+  // description,
+  // author: "A B",
+  // ogImage: "https://dev-lab-one.vercel.app/quizz/quizz-ipni.png",
+  // ogUrl: "https://dev-lab-one.vercel.app/quizz",
+  // ogType: "website",
+  // ogTitle: title,
+  // ogDescription: description,
+  // themeColor: "teal",
+});
+
+const picsInit = [
+  { src: 'quizz/art1.png', answer: 'Arthur'},
+  { src: 'quizz/art2.png', answer: 'Christophe'},
+  { src: 'quizz/art3.png', answer: 'Madeleine'},
+  { src: 'quizz/art4.png', answer: 'Ferdi'},
+  { src: 'quizz/art5.png', answer: 'Elisa'},
+  { src: 'quizz/art6.png', answer: 'Kevin'},
+  { src: 'quizz/art7.png', answer: 'Maël'},
+  { src: 'quizz/art8.png', answer: 'François'},
+  { src: 'quizz/art9.png', answer: 'Jimmy'},
+  { src: 'quizz/art10.png', answer: 'Mathilde'},
+]
+
+</script>
