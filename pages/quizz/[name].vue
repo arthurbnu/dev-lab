32a888e59512e1e57b918d4c49c60d1da6bd9a8f
arthurbnu <arthur.brody@bnu.fr>
2024-02-27 16:43:00 +0100

    404 handling

--- pages/quizz/[name].vue
+++ pages/quizz/[name].vue
@@ -12,9 +12,15 @@
 
 const route = useRoute()
 const { name } = route.params
+const event = useRequestEvent()
 const { data: quizzContent } = await useAsyncData("quizzContent", () =>
   queryContent("/quizz/" + name).findOne()
 );
+
+if (event && !quizzContent.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Aucun quizz trouvé à cette adresse' })
+}
+
 const sparqlRequest = quizzContent.value.body.children[0].props.code
 const baseUrl = "https://dev-lab-one.vercel.app" + route.fullPath.split(name)[0]
 const img =  quizzContent.value.image ?? "quizz-sparql.png" 
