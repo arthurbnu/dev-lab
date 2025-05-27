58bea3eec31ef477cbcfb57a91dbfebfd80a0a7c
arthurbnu <arthur.brody@bnu.fr>
2025-05-27 12:02:57 +0200

    Ajout d'un fond personnalisé et réorganisation du code dans atelier.vue

--- pages/atelier.vue
+++ pages/atelier.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="bg-teal-400/5 p-4 space-y-8">
+  <main class="bg-teal-400/5 p-4 space-y-8 my-main-bg">
     <h1 class="flex items-center gap-2 text-2xl">
       <UAvatar v-for="logo in logos" :src="'https://logo.clearbit.com/' + logo" class="w-12 h-12 mr-2 bg-white"
         size="md" />
@@ -24,17 +24,16 @@
     <section v-if="alsacianItems" class="">
       <h3 class="text-lg mb-2">Créatrices alsaciennes</h3>
       <div class="grid grid-cols-2 md:grid-cols-3 gap-4 my-animate-children-appear">
-        <div v-for="item in alsacianItems" :key="item.creatrice.value" class="bg-white p-4 rounded-lg shadow hover:scale-105 transition-transform">
+        <div v-for="item in alsacianItems" :key="item.creatrice.value"
+          class="bg-white p-4 rounded-lg shadow hover:scale-105 transition-transform">
 
           <a :href="item.article.value" target="_blank" class="text-blue-500 hover:underline">
-            {{ item.creatriceLabel?.value }} 
+            {{ item.creatriceLabel?.value }}
           </a>
 
           <p class="mt-2">
-            <img :src="item.image?.value ?? 'https://logo.clearbit.com/wikipedia.org'" 
-              :alt="item.creatriceLabel?.value"
-              loading="lazy"
-              class="w-full h-auto rounded-md" :class = "{'opacity-10': !item.image?.value}" />
+            <img :src="item.image?.value ?? 'https://logo.clearbit.com/wikipedia.org'" :alt="item.creatriceLabel?.value"
+              loading="lazy" class="w-full h-auto rounded-md" :class="{ 'opacity-10': !item.image?.value }" />
           </p>
           <p v-if="item.lieuNaissanceLabel?.value" class="text-sm text-gray-600 mt-1">
             Lieu de naissance: {{ item.lieuNaissanceLabel.value }}
@@ -47,28 +46,11 @@
       </pre>
     </section>
 
-    <!-- <section v-if="items" class="border-blue-300 border-solid border-l-4 pl-3 ">
-      <h3 class="text-lg mb-2">Dernières créatrices ajoutées / modifiées sur Wikidata</h3>
-      <ul v-auto-animate>
-        <li v-for="creatrice in items" :key="creatrice.creatrice.value" class="flex gap-10 justify-between">
-          <a :href="creatrice.creatrice.value" target="_blank">
-            {{ creatrice?.creatriceLabel?.value }}
-            <span class="hidden">
-              {{ creatrice?.date?.value }}
-            </span>
-          </a>
-          <span class="text-gray-500">
-            {{ new Date(creatrice?.modified?.value).toLocaleDateString() }}
-          </span>
-        </li>
-      </ul>
-    </section> -->
-
     <section class="hidden">
       {{ items }}
     </section>
 
-    <section class="my-10" v-if = "alsacianItems">
+    <section class="my-10" v-if="alsacianItems">
       <ContentList path="/atelier" v-slot="{ list }">
         <ContentQuery v-for="(item, id) in list" :key="item._path" :path="item._path" find="one" v-slot="{ data }">
           <ContentRenderer :value="data">
@@ -84,6 +66,23 @@
           <div class="w-1 h-10"></div>
         </ContentQuery>
       </ContentList>
+    </section>
+
+    <section v-if="items" class="border-blue-300 border-solid border-l-4 pl-3 ">
+      <h3 class="text-lg mb-2">Dernières créatrices ajoutées / modifiées sur Wikidata</h3>
+      <ul v-auto-animate>
+        <li v-for="creatrice in items" :key="creatrice.creatrice.value" class="flex gap-10 justify-between">
+          <a :href="creatrice.creatrice.value" target="_blank">
+            {{ creatrice?.creatriceLabel?.value }}
+            <span class="hidden">
+              {{ creatrice?.date?.value }}
+            </span>
+          </a>
+          <span class="text-gray-500">
+            {{ new Date(creatrice?.modified?.value).toLocaleDateString() }}
+          </span>
+        </li>
+      </ul>
     </section>
 
   </main>
@@ -220,7 +219,7 @@ ORDER BY (?statements)
 LIMIT 100
 `)
 
-const getLengthFromWikiApi = async (url) =>{
+const getLengthFromWikiApi = async (url) => {
   if (!url) {
     return 0; // Pas d'article, pas de longueur
   }
@@ -230,7 +229,7 @@ const getLengthFromWikiApi = async (url) =>{
   }
   const data = await response.json();
   const pageId = Object.keys(data.query.pages)[0];
-  return data.query.pages[pageId].length || 0; 
+  return data.query.pages[pageId].length || 0;
 }
 
 const fullUrlAlsacian = computed(() => endPoint + encodeURIComponent(alsacianCreatrices.value))
@@ -245,7 +244,7 @@ const { data: alsacianItems, execute: refreshAlsacian } = await useFetch(fullUrl
       articleLength: null
     }));
 
-     // On attend que toutes les longueurs soient récupérées
+    // On attend que toutes les longueurs soient récupérées
     await Promise.all(
       items.map(async (item) => {
         item.articleLength = await getLengthFromWikiApi(item.apiUrlForLength);
@@ -267,6 +266,14 @@ const { data: alsacianItems, execute: refreshAlsacian } = await useFetch(fullUrl
 
 </script>
 
+<style>
+body:has(.my-main-bg) {
+  background-color: #000000;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3Crect stroke='%23000000' stroke-width='0.8' width='1' height='1' id='s'/%3E%3Cpattern id='a' width='3' height='3' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cuse fill='%23242424' href='%23s' y='2'/%3E%3Cuse fill='%23242424' href='%23s' x='1' y='2'/%3E%3Cuse fill='%23333333' href='%23s' x='2' y='2'/%3E%3Cuse fill='%23333333' href='%23s'/%3E%3Cuse fill='%233e3e3e' href='%23s' x='2'/%3E%3Cuse fill='%233e3e3e' href='%23s' x='1' y='1'/%3E%3C/pattern%3E%3Cpattern id='b' width='7' height='11' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23484848'%3E%3Cuse href='%23s'/%3E%3Cuse href='%23s' y='5' /%3E%3Cuse href='%23s' x='1' y='10'/%3E%3Cuse href='%23s' x='2' y='1'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='8'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='5' y='2'/%3E%3Cuse href='%23s' x='5' y='6'/%3E%3Cuse href='%23s' x='6' y='9'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='h' width='5' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23484848'%3E%3Cuse href='%23s' y='5'/%3E%3Cuse href='%23s' y='8'/%3E%3Cuse href='%23s' x='1' y='1'/%3E%3Cuse href='%23s' x='1' y='9'/%3E%3Cuse href='%23s' x='1' y='12'/%3E%3Cuse href='%23s' x='2'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='2'/%3E%3Cuse href='%23s' x='3' y='6'/%3E%3Cuse href='%23s' x='3' y='11'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='4' y='10'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='c' width='17' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23515151'%3E%3Cuse href='%23s' y='11'/%3E%3Cuse href='%23s' x='2' y='9'/%3E%3Cuse href='%23s' x='5' y='12'/%3E%3Cuse href='%23s' x='9' y='4'/%3E%3Cuse href='%23s' x='12' y='1'/%3E%3Cuse href='%23s' x='16' y='6'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='d' width='19' height='17' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23000000'%3E%3Cuse href='%23s' y='9'/%3E%3Cuse href='%23s' x='16' y='5'/%3E%3Cuse href='%23s' x='14' y='2'/%3E%3Cuse href='%23s' x='11' y='11'/%3E%3Cuse href='%23s' x='6' y='14'/%3E%3C/g%3E%3Cg fill='%23585858'%3E%3Cuse href='%23s' x='3' y='13'/%3E%3Cuse href='%23s' x='9' y='7'/%3E%3Cuse href='%23s' x='13' y='10'/%3E%3Cuse href='%23s' x='15' y='4'/%3E%3Cuse href='%23s' x='18' y='1'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='e' width='47' height='53' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%2354E009'%3E%3Cuse href='%23s' x='2' y='5'/%3E%3Cuse href='%23s' x='16' y='38'/%3E%3Cuse href='%23s' x='46' y='42'/%3E%3Cuse href='%23s' x='29' y='20'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='f' width='59' height='71' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%2354E009'%3E%3Cuse href='%23s' x='33' y='13'/%3E%3Cuse href='%23s' x='27' y='54'/%3E%3Cuse href='%23s' x='55' y='55'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='g' width='139' height='97' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%2354E009'%3E%3Cuse href='%23s' x='11' y='8'/%3E%3Cuse href='%23s' x='51' y='13'/%3E%3Cuse href='%23s' x='17' y='73'/%3E%3Cuse href='%23s' x='99' y='57'/%3E%3C/g%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23a)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23b)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23h)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23c)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23d)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23e)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23f)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23g)' width='100%25' height='100%25'/%3E%3C/svg%3E");
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
 
 <style scoped>
 a:hover {
@@ -282,24 +289,24 @@ a:hover {
 }
 
 @keyframes appear {
-    from {
-        opacity: 0;
-        scale: .9
-    }
+  from {
+    opacity: 0;
+    scale: .9
+  }
 
-    to {
-        opacity: 1;
-        scale: 1
-    }
+  to {
+    opacity: 1;
+    scale: 1
+  }
 }
 
-.my-animate-children-appear{
-    padding-bottom: 200px;
+.my-animate-children-appear {
+  padding-bottom: 200px;
 }
 
 .my-animate-children-appear>div {
-    animation: appear linear both;
-    animation-timeline: view();
-    animation-range: entry 30% cover 25%;
+  animation: appear linear both;
+  animation-timeline: view();
+  animation-range: entry 30% cover 25%;
 }
 </style>
